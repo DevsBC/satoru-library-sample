40c48f1ef3a7485a98e6a3b8e203dbca0346c2a2
JuanCarlos Aranda <sistemas@injectronic.com>
2022-02-16 16:22:45 -0700

    angular elements build

--- projects/elements/src/app/app.module.ts
+++ projects/elements/src/app/app.module.ts
@@ -1,12 +1,23 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
+import { SatoruLibraryModule, TimeTrackerComponent } from 'satoru-library';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SatoruLibraryModule
   ],
   providers: [],
 })
-export class AppModule { }
+export class AppModule { 
+
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap(){
+    const element = createCustomElement(TimeTrackerComponent, { injector: this.injector })
+    customElements.define('sat-time-tracker', element);
+  }
+}
